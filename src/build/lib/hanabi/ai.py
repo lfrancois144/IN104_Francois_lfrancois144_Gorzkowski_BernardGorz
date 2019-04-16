e4399959fea2d246248e00c820a39d39a2333b98
BernardGorz <bernard.gorzkowski@ensta-paristech.fr>
2019-04-16 16:52:09 +0200

    results tracking.txt

--- src/build/lib/hanabi/ai.py
+++ src/build/lib/hanabi/ai.py
@@ -182,8 +182,10 @@ class BigBrain(AI):
             if (card.color_clue != False) and (card.number_clue != False):
                 card_color=str(card.color_clue)[0]
                 if game.piles.get(possible_colors.get(card_color)) == (int(card.number_clue) - 1) :
+                    print('Plays a safe card')
                     return("p"+str(i))
                 if game.piles.get(possible_colors.get(card_color)) > (int(card.number_clue) - 1) :
+                    print('Discards a safe card')
                     return("d"+str(i))
 
             i+=1
@@ -192,18 +194,35 @@ class BigBrain(AI):
 
         #Clues
         if game.blue_coins!=0:
-            for c in one_cards:
-                for card in game.hands[game.other_player].cards:
-                    if card.__eq__(c) and card.number_clue==False:
+            for card in game.hands[game.other_player].cards:
+                card_color=str(card.color)[0]
+
+                if card.number==5 and card.number_clue==False:
+                    print("Saw a 5, other player have to save it")
+                    return('c5')
+
+                if card.number==1:
+                    if card.number_clue==False:
                         print("Other player have a 1 and doesn't know it, must tell him")
                         return('c1')
 
-
-            for c in one_cards:
-                for card in game.hands[game.other_player].cards:
-                    if card.__eq__(c) and card.color_clue==False:
+                    if card.color_clue==False:
                         print("Other player knows he have a 1 but doesn't know the color")
                         return('c'+card_color)
+
+                top_card_number=game.piles.get(possible_colors.get(card_color))
+                if card.number==top_card_number+1:
+                    if card.number_clue==False:
+                        print("Giving a number clue about the "+str(card.color)+str(card.number)+", which can be played")
+                        return('c'+str(card.number))
+
+                    if card.color_clue==False:
+                        print("Giving a color clue about the "+str(card.color)+str(card.number)+", which can be played")
+                        return('c'+str(card.color))
+                    
+            print("Giving a random clue")
+            return('c'+random_list[randint(0,9)])
+                
 
 
         print('Plays randomly')
