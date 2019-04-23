6d577387b113e6d4461d9a1d1832bf0268fcfd76
Loic Francois <loic.francois@ensta-paristech.fr>
2019-04-23 18:02:28 +0200

    ébauche yolo

--- src/build/lib/hanabi/ai.py
+++ src/build/lib/hanabi/ai.py
@@ -191,11 +191,13 @@ class BigBrain(AI):
         color_lines={'R':0, 'B':1, 'G':2, 'W':3, 'Y':4}
         colors_in_game = [10]*5
         numbers_in_game = [ 15, 10, 10, 10, 5]
+#        deck_matrix = [[ 3, 2, 2, 2, 1]*5]
         for disc_card in game.discard_pile.cards:
             card_color_ind = color_lines.get(str(disc_card.color)[0])
             card_number_ind = int(disc_card.number)-1
             colors_in_game[card_color_ind] -=1
             numbers_in_game[card_number_ind] -=1
+#            deck_matrix[card_color_ind][card_number_ind] -=1
 
 
         #Playing cards, or including them in discard_list or do_not_discard
@@ -304,6 +306,11 @@ class BigBrain(AI):
             
 #            for card in game.current_hand.cards:
 #                if (card.color_clue != False) and (card.number_clue == False):
+
+        if game.red_coins<2 and game.blue_coins==8:
+            print("Yolo")
+            return('p'+random_list[randint(0,4)])
+
 
         if game.blue_coins<8:
             print("Discards a random card")
