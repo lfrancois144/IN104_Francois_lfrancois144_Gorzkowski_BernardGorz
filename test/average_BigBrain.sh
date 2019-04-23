2518810d5f55b1612b4fbaacd3e0c7cde7e61e5c
Loic Francois <loic.francois@ensta-paristech.fr>
2019-04-23 16:48:57 +0200

    update

--- test/average_BigBrain.sh
+++ test/average_BigBrain.sh
@@ -3,10 +3,10 @@
 nb=$*
 avg=$*
 max=$*
-min=$*
+#min=$*
 avg=0
 max=0
-min=25
+#min=25
 
 if [ -z "$nb" ]; then
     nb=500
@@ -18,13 +18,13 @@ for i in $(seq 1 $nb); do
     if [ $max -lt $score ]; then
         max=$score
     fi
-    if [ $score -lt $min ]; then
-        min=$score
-    fi
+#    if [ $score -lt $min ]; then
+#        min=$score
+#    fi
 done
 let "avg=$avg/$nb"
 
 echo tests=$nb
 echo avg=$avg | sed 's/..$/.&/'
 echo max=$max
-echo min=$min
+#echo min=$min
