610b7bb3755996169f31e3b8d8a9ae37a5f69241
Loic Francois <loic.francois@ensta-paristech.fr>
2019-04-23 16:55:42 +0200

    update

--- test/average_BigBrain.sh
+++ test/average_BigBrain.sh
@@ -3,10 +3,10 @@
 nb=$*
 avg=$*
 max=$*
-#min=$*
+
 avg=0
 max=0
-#min=25
+
 
 if [ -z "$nb" ]; then
     nb=500
@@ -14,17 +14,13 @@ fi
 
 for i in $(seq 1 $nb); do
     score=$(./src/hanabi/hanabi --ai=BigBrain |grep score |sed -e 's/.*is //')
-    let "avg=$avg+100*$score"
-    if [ $max -lt $score ]; then
+    let "avg=$avg+$100 * $score"
+    if [ "$max -lt $score" ]; then
         max=$score
     fi
-#    if [ $score -lt $min ]; then
-#        min=$score
-#    fi
 done
 let "avg=$avg/$nb"
 
 echo tests=$nb
 echo avg=$avg | sed 's/..$/.&/'
 echo max=$max
-#echo min=$min
