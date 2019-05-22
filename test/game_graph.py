d305601548b54b534e1af86fe33eb45f14b0727b
BernardGorz <bernard.gorzkowski@ensta-paristech.fr>
2019-05-22 19:24:08 +0200

    Finished graphing games

--- test/game_graph.py
+++ test/game_graph.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import contextlib
 
-iter = 500
+iter = 10000
 
-possible_scores = [i for i in range(26)]
-occurences = [0 for i in range(26)]
+possible_scores = np.array([i for i in range(26)])
+occurences = np.array([0 for i in range(26)])
 
 with contextlib.redirect_stdout(None):
 
@@ -33,16 +33,41 @@ while i >= 0 and occurences[i] == 0:
     i -= 1
     max = i
 
-possible_scores = possible_scores[min:max+1]
-occurences = occurences[min:max+1]
+
+avg = 0
+for i in possible_scores:
+    x = occurences[i]
+    avg = avg + x*i
+avg = avg/iter
+
+sd = 0
+for i in possible_scores:
+    x = occurences[i]
+    sd = sd + x*((i-avg)**2)
+sd = np.sqrt(sd/iter)
+
+median = -1
+sum = 0
+for i in possible_scores:
+    x = occurences[i]
+    sum = sum + x
+    if median == -1 and sum > iter/2:
+        median = i
+
+occurences = occurences/iter
+
+X = np.linspace(0,25,10000)
+Y = np.exp(-(1/2)*((X-avg)/sd)**2)/(sd*np.sqrt(2*np.pi))
+plt.plot(X,Y, color = [41/255, 163/255, 41/255], linewidth = 2)
 
 def plot_bar_x():
     # this is for plotting purpose
     index = np.arange(len(possible_scores))
-    plt.bar(index, occurences)
+    plt.bar(index, occurences, alpha = 0.5, color = [48/255,96/255,255/255], edgecolor = [24/255,48/255,255/255], linewidth = 2)
     plt.xlabel('Scores', fontsize=15)
-    plt.ylabel('Occurences', fontsize=15)
-    plt.xticks(index, possible_scores, fontsize=10, rotation=30)
+    plt.ylabel('Occurences (normalized)', fontsize=15)
+    plt.xticks(index, possible_scores, fontsize=7, rotation=30)
+    plt.text(0, 0.2, 'Games = '+str(iter)+'\nMedian = '+str(median)+'\nAverage = '+str(round(avg,2))+'\nStandard deviation = '+str(round(sd,2))+'\nMin = '+str(min)+'\nMax = '+str(max), fontsize=10, horizontalalignment='left', verticalalignment='top')
     plt.title('Times the AI hit different scores', fontsize=20)
     plt.show()
 
